974e329b9f8dd007fe3250ede32f3df610f0e7d3
Emilia Kura <emillia.kura@gmail.com>
2026-07-27 14:33:59 +0200

    feat: add handleLogin function
    - Now it only logs data in console.

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -3,4 +3,10 @@ import {useState} from "react";
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // TODO: call backend
+        console.log("Data: ", {email, password});
+    }
+
 }
