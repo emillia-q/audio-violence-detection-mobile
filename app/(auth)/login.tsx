941b40b08317747cff960cbf1da9b2fcc8d11f70
Emilia Kura <emillia.kura@gmail.com>
2026-07-27 15:50:49 +0200

    refactor: aligned items to phone theme

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,15 @@
 import {useState} from "react";
-import {Text, TextInput, View, StyleSheet} from "react-native";
+import {Text, TextInput, View, StyleSheet, useColorScheme} from "react-native";
+import {Colors} from "@/src/constants/theme";
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    // Get phone theme
+    const theme = useColorScheme() ?? 'light';
+    // Get appropriate color package
+    const activeColors = Colors[theme];
 
     const handleLogin = () => {
         // TODO: call backend
@@ -11,19 +17,20 @@ export default function Login() {
     }
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Audio Violence Detection</Text>
+        <View style={[styles.container, {backgroundColor: activeColors.background}]}>
+            <Text style={[styles.title, {color: activeColors.text}]}>
+                Audio Violence Detection</Text>
 
             {/* Inputs */}
             <TextInput
-                style={styles.input}
+                style={[styles.input, {color: activeColors.text, borderColor: activeColors.icon}]}
                 placeholder={"E-mail"}
                 value={email}
                 onChangeText={setEmail}
                 autoCapitalize={"none"}
             />
             <TextInput
-                style={styles.input}
+                style={[styles.input, {color: activeColors.text, borderColor: activeColors.icon}]}
                 placeholder={"Password"}
                 value={password}
                 onChangeText={setPassword}
@@ -34,11 +41,11 @@ export default function Login() {
 }
 
 const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       justifyContent: 'center',
-       padding: 20,
-   },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+    },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
@@ -46,8 +53,7 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     input: {
-       borderWidth: 1,
-        borderColor: '#ccc',
+        borderWidth: 1,
         padding: 10,
         borderRadius: 5,
         marginBottom: 10,
