f97407b92b86b11883ecbcdb8b712607c19e4aa5
Emilia Kura <emillia.kura@gmail.com>
2026-07-27 15:58:01 +0200

    feat: add & defined border & placeholder to colors map

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -16,6 +16,8 @@ export const Colors = {
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    border: '#cccccc',
+    placeholder: '#888888',
   },
   dark: {
     text: '#ECEDEE',
@@ -24,6 +26,8 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    border: '#444444',
+    placeholder: '#aaaaaa',
   },
 };
 
