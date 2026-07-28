b2d6629a434bdf1b5e8155937759e97b0edc360a
Emilia Kura <emillia.kura@gmail.com>
2026-07-28 11:22:46 +0200

    feat: moved logic from login.tsx to CustomInput.tsx with styles

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,4 +1,4 @@
-import {TextInputProps} from "react-native";
+import {StyleSheet, TextInput, TextInputProps} from "react-native";
 import {Colors} from "@/src/constants/theme";
 
 interface CustomInputProps extends TextInputProps {}
@@ -6,5 +6,28 @@ interface CustomInputProps extends TextInputProps {}
 export function CustomInput(props: CustomInputProps) {
     const activeColors = Colors.default;
 
-
+    return (
+        <TextInput
+            style={[
+                styles.input,
+                {
+                    color: activeColors.text,
+                    borderColor: activeColors.border,
+                    backgroundColor: 'rgba(255,255,255,0.05)'
+                },
+                props.style // Allows to override
+            ]}
+            placeholderTextColor={activeColors.placeholder}
+            {...props}
+        />
+    );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        padding: 15,
+        borderRadius: 12,
+        fontSize: 16,
+    }
+})
