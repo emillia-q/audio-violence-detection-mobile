6553b38df1185057e9b5f0563ffdadb2ab21e2c3
Emilia Kura <emillia.kura@gmail.com>
2026-07-28 11:17:11 +0200

    feat: create CustomInput.tsx
    - To reduce code in login.tsx and move shared ui style to distinct component.

---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,10 @@
+import {TextInputProps} from "react-native";
+import {Colors} from "@/src/constants/theme";
+
+interface CustomInputProps extends TextInputProps {}
+
+export function CustomInput(props: CustomInputProps) {
+    const activeColors = Colors.default;
+
+
+}
