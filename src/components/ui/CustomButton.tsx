10573e2af3ab358312618d39dafd07019bc9488f
Emilia Kura <emillia.kura@gmail.com>
2026-07-28 11:48:55 +0200

    feat: add styles in CustomButton.tsx

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -1,9 +1,27 @@
-import {TouchableOpacityProps} from "react-native";
+import {StyleSheet, TouchableOpacityProps} from "react-native";
+import {Colors} from "@/src/constants/theme";
 
 interface CustomButtonProps extends TouchableOpacityProps {
     title: string;
 }
 
 export function CustomButton({style, title, ...rest}: CustomButtonProps) {
+    const activeColors = Colors.default;
 
+    return (
+
+    );
 }
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 16,
+        borderRadius: 12,
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        elevation: 5,
+    }
+})
