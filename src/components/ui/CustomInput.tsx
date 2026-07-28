23e7e9e31f144069850820bcf276131530fcbd94
Emilia Kura <emillia.kura@gmail.com>
2026-07-28 19:56:05 +0200

    feat: handle onFocus and onBlur events in CustomInput

--- src/components/ui/CustomInput.tsx
+++ src/components/ui/CustomInput.tsx
@@ -1,33 +1,57 @@
-import {StyleSheet, TextInput, TextInputProps} from "react-native";
+import {StyleSheet, TextInput, TextInputProps, View} from "react-native";
 import {Colors} from "@/src/constants/theme";
+import {useState} from "react";
 
 interface CustomInputProps extends TextInputProps {}
 
-export function CustomInput({style, ...rest}: CustomInputProps) {
+export function CustomInput({style, secureTextEntry, onFocus, onBlur,  ...rest}: CustomInputProps) {
     const activeColors = Colors.default;
 
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
-        <TextInput
-            {...rest}
-            placeholderTextColor={activeColors.placeholder}
-            style={[
-                styles.input,
-                {
-                    color: activeColors.text,
-                    borderColor: activeColors.border,
-                    backgroundColor: 'rgba(255,255,255,0.05)'
-                },
-                style
-            ]}
-        />
+        <View style={[
+            styles.container,
+            {
+                backgroundColor: activeColors.surface,
+                borderColor: isFocused ? activeColors.tint : activeColors.border,
+            },
+            style
+        ]}>
+            <TextInput
+                {...rest}
+                placeholderTextColor={activeColors.placeholder}
+
+                // Toggle focus
+                onFocus={(e) => {
+                    setIsFocused(true);
+                    if (onFocus) onFocus(e);
+                }}
+                onBlur={(e) => {
+                    setIsFocused(false);
+                    if (onBlur) onBlur(e);
+                }}
+                style={[
+                    styles.input,
+                    {color: activeColors.text}
+                ]}
+            />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1.5,
+        borderRadius: 14,
+        overflow: 'hidden',
+    },
     input: {
-        borderWidth: 1,
-        padding: 15,
-        borderRadius: 12,
+        flex: 1,
+        paddingVertical: 16,
+        paddingHorizontal: 16,
         fontSize: 16,
     }
 })
