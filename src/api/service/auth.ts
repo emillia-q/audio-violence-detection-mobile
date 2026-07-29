17c18f1a6efae39a167f1a3b50cf8a7a9f998e33
Emilia Kura <emillia.kura@gmail.com>
2026-07-29 20:20:56 +0200

    feat: add shared path to make code more dry

--- src/api/service/auth.ts
+++ src/api/service/auth.ts
@@ -3,14 +3,16 @@ import {AuthResponse} from "@/src/api/dto/response/AuthResponse";
 import {apiClient} from "@/src/api/client";
 import {RegisterRequest} from "@/src/api/dto/request/RegisterRequest";
 
+const PATH = '/auth'
+
 export const authService = {
     login: async (data: LoginRequest): Promise<AuthResponse> => {
-        const response = await apiClient.post<AuthResponse>('/auth/login', data);
+        const response = await apiClient.post<AuthResponse>(`${PATH}/login`, data);
         return response.data;
     },
 
     register: async (data: RegisterRequest): Promise<AuthResponse> => {
-        const response = await apiClient.post<AuthResponse>('/auth/register', data);
+        const response = await apiClient.post<AuthResponse>('${PATH}/register', data);
         return response.data;
     }
 }
