221e77ffe0cbafbe3d67dedcb93d4dd4df16192a
Emilia Kura <emillia.kura@gmail.com>
2026-07-29 20:18:57 +0200

    feat: add register function in authservice

--- src/api/service/auth.ts
+++ src/api/service/auth.ts
@@ -1,10 +1,16 @@
 import {LoginRequest} from "@/src/api/dto/request/LoginRequest";
 import {AuthResponse} from "@/src/api/dto/response/AuthResponse";
 import {apiClient} from "@/src/api/client";
+import {RegisterRequest} from "@/src/api/dto/request/RegisterRequest";
 
 export const authService = {
     login: async (data: LoginRequest): Promise<AuthResponse> => {
         const response = await apiClient.post<AuthResponse>('/auth/login', data);
+        return response.data;
+    },
+
+    register: async (data: RegisterRequest): Promise<AuthResponse> => {
+        const response = await apiClient.post<AuthResponse>('/auth/register', data);
         return response.data;
     }
 }
