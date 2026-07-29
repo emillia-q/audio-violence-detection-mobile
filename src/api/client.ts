4614cc5173d6e82064fc07c16f31f021b3bf1fc8
Emilia Kura <emillia.kura@gmail.com>
2026-07-29 14:53:32 +0200

    fix: url api prefix
    - In order for Expo to allow an app on your phone to read this address, the variable name must start with the words EXPO_PUBLIC_

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_URL;
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const apiClient = axios.create({
    baseURL: BASE_URL,
