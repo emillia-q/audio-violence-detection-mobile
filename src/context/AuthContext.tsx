e0cdd19463f66c9a4c08bb15364b6aab0d60fe7e
Emilia Kura <emillia.kura@gmail.com>
2026-07-30 10:33:45 +0200

    feat: create context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,9 @@
+import {createContext} from "react";
+
 interface AuthContextType {
     token: string | null;
     login: (token: string) => Promise<void>;
     logout: () => Promise<void>;
 }
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
