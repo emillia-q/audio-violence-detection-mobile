5e3deb8a2490bfe7dfddd0383964ec7b5b41e2bc
Emilia Kura <emillia.kura@gmail.com>
2026-07-30 13:28:12 +0200

    feat: add redirection to home screen or to login screen if the user doesn't have token saved in storage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { DarkTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import {Stack, useRouter, useSegments} from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { Colors } from '@/src/constants/theme';
 import {AuthProvider, useAuth} from "@/src/context/AuthContext";
+import {useEffect} from "react";
 
 const CustomDarkTheme = {
     ...DarkTheme,
@@ -17,7 +18,25 @@ const CustomDarkTheme = {
 };
 
 function RootLayoutNav() {
-    const {token} = useAuth();
+    const {token, loading} = useAuth();
+
+    // Init expo router tools
+    const router = useRouter();
+    const segments = useSegments();
+
+    useEffect(() => {
+        if (loading)
+            return;
+
+        // Check if the user is at login section
+        const inAuthGroup = segments[0] === '(auth)';
+
+        // If user has token -> redirect to his home screen and if not redirect to login screen
+        if (!token && !inAuthGroup)
+            router.replace('/(auth)/login');
+        else if (token && inAuthGroup)
+            router.replace('/(tabs)');
+    }, [token, loading, segments]);
 
     return (
         <ThemeProvider value={CustomDarkTheme}>
