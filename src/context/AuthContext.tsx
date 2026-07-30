87968e5718827aff53bb21070e59de835831743a
Emilia Kura <emillia.kura@gmail.com>
2026-07-30 11:07:34 +0200

    feat: add logic to login function
    - It saves token to phone store & to cache.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import {createContext, ReactNode, useState} from "react";
+import * as SecureStore from 'expo-secure-store';
 
 interface AuthContextType {
     token: string | null;
@@ -11,11 +12,19 @@ export const AuthContext = createContext<AuthContextType | undefined>(undefined)
 export const AuthProvider = ({children}: {children: ReactNode}) => {
     const [token, setToken] = useState<string | null>(null);
 
+    // Called after successful login
+    const login = async (token: string) => {
+        // Save token in store
+        await SecureStore.setItemAsync('token', token);
+        // Save token in cache
+        setToken(token);
+    }
+
     return (
         <AuthContext.Provider value={{
             token,
-            login: async () => {},
-            logout: async () => {}
+            login,
+            logout
         }}>
             {children}
         </AuthContext.Provider>
