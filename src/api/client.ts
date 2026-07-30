7dc3cfba8c25ae6c1a9f006d60259bccd77d9f17
Emilia Kura <emillia.kura@gmail.com>
2026-07-30 13:39:44 +0200

    feat: add interceptor
    - To protect against sending request without a token.

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import * as SecureStore from 'expo-secure-store'
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+const TOKEN_KEY = 'token';
 
 export const apiClient = axios.create({
    baseURL: BASE_URL,
@@ -8,6 +10,21 @@ export const apiClient = axios.create({
        'Content-Type': 'application/json',
    },
     timeout: 10000,
-
-    // TODO: add token support
 });
+
+// Protection against sending request without a token
+apiClient.interceptors.request.use(
+    async (config) => {
+        try {
+            const token = await SecureStore.getItemAsync(TOKEN_KEY);
+            if (token)
+                config.headers.Authorization = `Bearer ${token}`;
+        } catch (error) {
+            console.error("Error while downloading token from SecureStore", error);
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
