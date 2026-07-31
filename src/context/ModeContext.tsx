3647d718a634cd329d5c95bb885ab78ea3d632dc
Emilia Kura <emillia.kura@gmail.com>
2026-07-31 13:10:43 +0200

    feat: add mode context and mode provider

--- src/context/ModeContext.tsx
+++ src/context/ModeContext.tsx
@@ -1,6 +1,20 @@
+import {createContext, useState} from "react";
+
 export type AppMode = 'user' | 'trustedUser'
 
 interface ModeContextData {
     mode: AppMode;
     setMode: (mode: AppMode) => void;
+}
+
+const ModeContext = createContext<ModeContextData | undefined>(undefined);
+
+export function ModeProvider({children}: {children: React.ReactNode}) {
+    const [mode, setMode] = useState<AppMode>('user'); // Load user as domain
+
+    return (
+        <ModeContext.Provider value={{mode, setMode}}>
+            {children}
+        </ModeContext.Provider>
+    );
 }
