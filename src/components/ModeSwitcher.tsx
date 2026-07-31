508113cbe4d4a9a67613ddbe54629c76ff4d00b4
Emilia Kura <emillia.kura@gmail.com>
2026-07-31 11:54:06 +0200

    feat: add ModeSwitcher function & style sheet

--- src/components/ModeSwitcher.tsx
+++ src/components/ModeSwitcher.tsx
@@ -1,4 +1,24 @@
+import {StyleSheet, View} from "react-native";
+import {Colors} from "@/src/constants/theme";
+
 interface ModeSwitcherProps {
     selectedIndex: number;
     onSelect: (index: number) => void;
 }
+
+export default function ModeSwitcher({selectedIndex, onSelect}: ModeSwitcherProps) {
+    const isUserMode = selectedIndex === 0;
+
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+   container: {
+       padding: 16,
+       backgroundColor: Colors.default.background,
+   }
+});
