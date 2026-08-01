5ac1a3f874ec761e39c3c3ad455aaf826d93f0e3
Emilia Kura <emillia.kura@gmail.com>
2026-08-01 18:32:53 +0200

    feat: add getStatusColor function

--- src/components/dashboard/shared/StatusBadge.tsx
+++ src/components/dashboard/shared/StatusBadge.tsx
@@ -1,3 +1,5 @@
+import {Colors} from "@/src/constants/theme";
+
 type StatusType = 'online' | 'offline' | 'warning';
 
 interface StatusBadgeProps {
@@ -5,6 +7,20 @@ interface StatusBadgeProps {
     text: string;
 }
 
-export default function StatusBadge() {
+export default function StatusBadge({status, text}: StatusBadgeProps) {
+    // Set color based on status
+    const getStatusColor = () => {
+        switch (status) {
+            case "online":
+                return '#10B981';
+            case "offline":
+                return '#64748B';
+            case "warning":
+                return '#EF4444';
+            default:
+                return Colors.default.muted;
+        }
+    }
 
+    const color = getStatusColor();
 }
