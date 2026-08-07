6320b8525063650ff0038fc12e7cd556ba1d7ac4
Emilia Kura <emillia.kura@gmail.com>
2026-08-07 19:13:12 +0200

    refactor: replace view tag with custom EmptyListView

--- src/components/dashboard/user/trusted-users/TrustedUserList.tsx
+++ src/components/dashboard/user/trusted-users/TrustedUserList.tsx
@@ -2,6 +2,7 @@ import {TrustedUserListResponse} from "@/src/api/dto/response/TrustedUserListRes
 import {StyleSheet, Text, View} from "react-native";
 import {Colors} from "@/src/constants/theme";
 import NavigationCard from "@/src/components/ui/NavigationCard";
+import EmptyListView from "@/src/components/ui/EmptyListView";
 
 interface TrustedUserListProps {
     trustedUsers: TrustedUserListResponse[];
@@ -11,11 +12,7 @@ export default function TrustedUserList({trustedUsers}: TrustedUserListProps) {
     // 204 - empty list
     if (trustedUsers.length === 0) {
         return (
-            <View style={styles.emptyContainer}>
-                <Text style={styles.emptyTitle}>
-                    No trusted users
-                </Text>
-            </View>
+            <EmptyListView title={'No trusted users'}/>
         );
     }
 
@@ -34,20 +31,6 @@ export default function TrustedUserList({trustedUsers}: TrustedUserListProps) {
 }
 
 const styles = StyleSheet.create({
-    emptyContainer: {
-        alignItems: 'center',
-        padding: 24,
-        backgroundColor: Colors.user.surface,
-        borderColor: Colors.user.border,
-        borderWidth: 1,
-        borderRadius: 16,
-        borderStyle: 'dashed',
-    },
-    emptyTitle: {
-        color: Colors.user.text,
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
     listContainer: {
         width: '100%',
     },
