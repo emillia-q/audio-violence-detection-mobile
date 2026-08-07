c3347b76afd9411e6c7c41de9715eebec2855c0e
Emilia Kura <emillia.kura@gmail.com>
2026-08-07 20:05:01 +0200

    fix: path name in user service

--- src/api/service/user.ts
+++ src/api/service/user.ts
@@ -1,11 +1,11 @@
 import {TrustedUserListResponse} from "@/src/api/dto/response/TrustedUserListResponse";
 import {apiClient} from "@/src/api/client";
 
-const PATH = '/trusted-users'
+const PATH = '/users'
 
 export const userService = {
     getListOfTrustedUsers: async (): Promise<TrustedUserListResponse[]> => {
-        const response = await apiClient.get(`${PATH}`);
+        const response = await apiClient.get(`${PATH}/trusted-users`);
 
         // 204 support
         if (response.status === 204)
