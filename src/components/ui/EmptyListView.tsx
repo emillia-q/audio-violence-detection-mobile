bb969da943941097d149290d5126422e4b51cf01
Emilia Kura <emillia.kura@gmail.com>
2026-08-07 19:07:21 +0200

    feat: add styles

--- src/components/ui/EmptyListView.tsx
+++ src/components/ui/EmptyListView.tsx
@@ -1,4 +1,30 @@
+import {StyleSheet} from "react-native";
+import {Colors} from "@/src/constants/theme";
+
 interface EmptyListViewProps {
     title: string;
 }
 
+export default function EmptyListView({title}: EmptyListViewProps) {
+
+}
+
+const styles = StyleSheet.create({
+    emptyContainer: {
+        alignItems: 'center',
+        padding: 24,
+        backgroundColor: Colors.user.surface,
+        borderColor: Colors.user.border,
+        borderWidth: 1,
+        borderRadius: 16,
+        borderStyle: 'dashed',
+    },
+    emptyTitle: {
+        color: Colors.user.text,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    listContainer: {
+        width: '100%',
+    },
+});
