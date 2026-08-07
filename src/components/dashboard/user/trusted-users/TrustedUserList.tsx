9adbc8e34ecb18901df5346c62aa6b6490bd0382
Emilia Kura <emillia.kura@gmail.com>
2026-08-07 19:02:02 +0200

    feat: add support for empty list and list of trusted users

--- src/components/dashboard/user/trusted-users/TrustedUserList.tsx
+++ src/components/dashboard/user/trusted-users/TrustedUserList.tsx
@@ -1,13 +1,36 @@
 import {TrustedUserListResponse} from "@/src/api/dto/response/TrustedUserListResponse";
-import {StyleSheet} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 import {Colors} from "@/src/constants/theme";
+import NavigationCard from "@/src/components/ui/NavigationCard";
 
 interface TrustedUserListProps {
     trustedUsers: TrustedUserListResponse[];
 }
 
 export default function TrustedUserList({trustedUsers}: TrustedUserListProps) {
+    // 204 - empty list
+    if (trustedUsers.length === 0) {
+        return (
+            <View style={styles.emptyContainer}>
+                <Text style={styles.emptyTitle}>
+                    No trusted users
+                </Text>
+            </View>
+        );
+    }
 
+    // 200 - list of trusted users
+    return (
+        <View style={styles.listContainer}>
+            {trustedUsers.map((trustedUser) => (
+                <NavigationCard
+                    key={trustedUser.trustedUserId}
+                    title={trustedUser.trustedUserNickname}
+                    onPress={() => console.log(`Trusted user with id: ${trustedUser.trustedUserId}`)}
+                />
+            ))}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
