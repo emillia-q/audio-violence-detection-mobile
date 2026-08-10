a347ac2ac6137d0a6edf59d2891027ac35880f3b
Emilia Kura <emillia.kura@gmail.com>
2026-08-10 22:14:05 +0200

    feat: add handlePress method

--- src/components/dashboard/shared/LogoutButton.tsx
+++ src/components/dashboard/shared/LogoutButton.tsx
@@ -1,4 +1,28 @@
+import {useTheme} from "@/src/context/ModeContext";
+import {Alert} from "react-native";
+
 interface LogoutButtonProps {
     onLogout: () => void;
 }
 
+export default function LogoutButton({onLogout}: LogoutButtonProps) {
+    const theme = useTheme();
+
+    const handlePress = () => {
+        Alert.alert(
+            "Log Out",
+            "Are you sure you want to log out?",
+            [
+                {
+                    text: "Cancel",
+                    style: "cancel"
+                },
+                {
+                    text: "Log Out",
+                    style: "destructive",
+                    onPress: onLogout
+                }
+            ]
+        )
+    }
+}
