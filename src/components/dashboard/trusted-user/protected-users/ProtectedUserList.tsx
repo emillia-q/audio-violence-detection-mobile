1ccf8ff0a818ab224c9aedfb47798814ec025013
Emilia Kura <emillia.kura@gmail.com>
2026-08-11 21:34:02 +0200

    feat: add 204 support

--- src/components/dashboard/trusted-user/protected-users/ProtectedUserList.tsx
+++ src/components/dashboard/trusted-user/protected-users/ProtectedUserList.tsx
@@ -1,12 +1,18 @@
 import {ProtectedUserListResponse} from "@/src/api/dto/response/ProtectedUserListResponse";
 import {StyleSheet} from "react-native";
+import EmptyListView from "@/src/components/ui/EmptyListView";
 
 interface ProtectedUserListProps {
     protectedUsers: ProtectedUserListResponse[];
 }
 
 export default function ProtectedUserList({protectedUsers}: ProtectedUserListProps) {
-
+    // 204
+    if (protectedUsers.length === 0) {
+        return (
+            <EmptyListView title={"No protected users"}/>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
