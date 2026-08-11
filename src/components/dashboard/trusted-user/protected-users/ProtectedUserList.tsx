fa2e0362d6c1174cb81a2dc9abf2b6bd8577b000
Emilia Kura <emillia.kura@gmail.com>
2026-08-11 21:41:04 +0200

    feat: add list view support

--- src/components/dashboard/trusted-user/protected-users/ProtectedUserList.tsx
+++ src/components/dashboard/trusted-user/protected-users/ProtectedUserList.tsx
@@ -1,6 +1,7 @@
 import {ProtectedUserListResponse} from "@/src/api/dto/response/ProtectedUserListResponse";
-import {StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
 import EmptyListView from "@/src/components/ui/EmptyListView";
+import NavigationCard from "@/src/components/ui/NavigationCard";
 
 interface ProtectedUserListProps {
     protectedUsers: ProtectedUserListResponse[];
@@ -13,6 +14,18 @@ export default function ProtectedUserList({protectedUsers}: ProtectedUserListPro
             <EmptyListView title={"No protected users"}/>
         );
     }
+
+    return (
+        <View style={styles.listContainer}>
+            {protectedUsers.map((protectedUser) => (
+                <NavigationCard
+                    key={protectedUser.protectedUserId}
+                    title={protectedUser.protectedUserNickname}
+                    onPress={() => console.log("Protected user with Id:", protectedUser.protectedUserId)}
+                />
+            ))}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
