fb6a596c4b0f3d9272eb8fe38e093d028c9da25e
Emilia Kura <emillia.kura@gmail.com>
2026-08-12 14:28:41 +0200

    feat: add icon and button to empty view

--- src/components/ui/EmptyListView.tsx
+++ src/components/ui/EmptyListView.tsx
@@ -1,12 +1,22 @@
 import {StyleSheet, Text, View} from "react-native";
 import {Colors} from "@/src/constants/theme";
 import {useTheme} from "@/src/context/ModeContext";
+import {Ionicons} from "@expo/vector-icons";
+import {CustomButton} from "@/src/components/ui/CustomButton";
 
 interface EmptyListViewProps {
     title: string;
+    iconName?: keyof typeof Ionicons.glyphMap;
+    buttonTitle?: string;
+    onButtonPress?: () => void;
 }
 
-export default function EmptyListView({title}: EmptyListViewProps) {
+export default function EmptyListView({
+                                          title,
+                                          iconName,
+                                          buttonTitle,
+                                          onButtonPress
+                                      }: EmptyListViewProps) {
     const theme = useTheme();
 
     return (
@@ -14,10 +24,21 @@ export default function EmptyListView({title}: EmptyListViewProps) {
             styles.container,
             {
                 backgroundColor: theme.background,
-                borderColor: theme.muted
+                borderColor: theme.border
             }
         ]}
         >
+            {/* Optional icon */}
+            {iconName && (
+                <Ionicons
+                    name={iconName}
+                    size={48}
+                    color={theme.muted}
+                    style={styles.icon}
+                />
+            )}
+
+            {/* Title */}
             <Text style={[
                 styles.title,
                 {
@@ -27,6 +48,17 @@ export default function EmptyListView({title}: EmptyListViewProps) {
             >
                 {title}
             </Text>
+
+            {/* Optional button in center */}
+            {buttonTitle && onButtonPress && (
+                <View style={styles.button}>
+                    <CustomButton
+                        title={buttonTitle}
+                        variant={"outline"}
+                        onPress={onButtonPress}
+                    />
+                </View>
+            )}
         </View>
     );
 }
@@ -40,8 +72,17 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         borderStyle: 'dashed',
     },
+    icon: {
+        marginBottom: 12,
+    },
     title: {
         fontSize: 15,
         fontWeight: '500',
+        textAlign: 'center',
+    },
+    button: {
+        marginTop: 24,
+        width: '100%',
+        maxWidth: 220,
     },
 });
