f2cc39c946603939d566c9f37f99892fe14aefc5
Emilia Kura <emillia.kura@gmail.com>
2026-08-12 10:35:32 +0200

    feat: add styles & view container

--- src/components/dashboard/shared/DashboardSection.tsx
+++ src/components/dashboard/shared/DashboardSection.tsx
@@ -1,7 +1,25 @@
 import {ReactNode} from "react";
+import {useTheme} from "@/src/context/ModeContext";
+import {StyleSheet, View} from "react-native";
 
 interface DashboardSectionProps {
     title: string;
     children: ReactNode;
     actionButton?: ReactNode;
 }
+
+export default function DashboardSection({title, children, actionButton}: DashboardSectionProps) {
+    const theme = useTheme();
+
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 8,
+    },
+});
