11a580da5c2f3c75d9be0bd7b2cba2f24a3be6ef
Emilia Kura <emillia.kura@gmail.com>
2026-08-13 13:10:47 +0200

    feat: add label above input component

---
+++ src/components/ui/AboveInputLabel.tsx
@@ -0,0 +1,34 @@
+import {useTheme} from "@/src/context/ModeContext";
+import {StyleSheet, Text} from "react-native";
+import {Colors} from "@/src/constants/theme";
+
+interface AboveInputLabelProps {
+    title: string;
+}
+
+export default function AboveInputLabel({title}: AboveInputLabelProps) {
+    const theme = useTheme();
+
+    return (
+        <Text style={[
+            styles.label,
+            {
+                color: theme.muted
+            }
+        ]}
+        >
+            {title}
+        </Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    label: {
+        fontSize: 13,
+        fontWeight: '700',
+        marginLeft: 4,
+        marginBottom: 8,
+        textTransform: 'uppercase',
+        letterSpacing: 0.8,
+    },
+});
