5e06a7c0c67fe9d2d0b6d821a2a0bce23f3c0e97
Emilia Kura <emillia.kura@gmail.com>
2026-08-13 11:31:52 +0200

    feat: add handleSubmit function

--- src/components/dashboard/user/trusted-users/AddTrustedUserSheet.tsx
+++ src/components/dashboard/user/trusted-users/AddTrustedUserSheet.tsx
@@ -1,6 +1,23 @@
+import {useState} from "react";
+
 interface AddTrustedUserSheetProps {
     isVisible: boolean;
     onClose: () => void;
     onSubmit: (email: string, nickname: string) => void;
 }
 
+export default function AddTrustedUserSheet({isVisible, onClose, onSubmit}: AddTrustedUserSheetProps) {
+    const [email, setEmail] = useState("");
+    const [nickname, setNickname] = useState("");
+
+    const handleSubmit = () => {
+        const trimmedEmail = email.trim();
+        const trimmedNickname = nickname.trim();
+
+        onSubmit(trimmedEmail, trimmedNickname);
+
+        // Clear form after data are sent
+        setEmail("");
+        setNickname("");
+    }
+}
