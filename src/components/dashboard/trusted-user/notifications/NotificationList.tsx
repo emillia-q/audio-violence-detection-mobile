36d180d857e391cab24df8c16312e5e8470f0ee7
Emilia Kura <emillia.kura@gmail.com>
2026-08-19 13:08:07 +0200

    feat: add empty list support

--- src/components/dashboard/trusted-user/notifications/NotificationList.tsx
+++ src/components/dashboard/trusted-user/notifications/NotificationList.tsx
@@ -1,12 +1,21 @@
 import {NotificationListResponse} from "@/src/api/dto/response/NotificationListResponse";
 import {StyleSheet} from "react-native";
+import EmptyListView from "@/src/components/ui/EmptyListView";
 
 interface NotificationListProps {
     notifications: NotificationListResponse[];
 }
 
 export default function NotificationList({notifications}: NotificationListProps) {
-
+    // 204 - empty list
+    if (notifications.length === 0) {
+        return (
+            <EmptyListView
+                title={'No recent notifications'}
+                iconName={"notifications-off-outline"}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
