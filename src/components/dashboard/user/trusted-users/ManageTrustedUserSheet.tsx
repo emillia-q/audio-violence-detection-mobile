05c1e0192bcebf1a2a4d26a72e9145ae7ef5903c
Emilia Kura <emillia.kura@gmail.com>
2026-08-19 23:02:07 +0200

    refactor: allow empty string to delete current nickname

--- src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
+++ src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
@@ -15,7 +15,7 @@ import {CustomButton} from "@/src/components/ui/CustomButton";
 const formSchema = z.object({
     nickname: z.string()
         .max(100, "Nickname cannot be longer than 100 characters")
-        .regex(/^(?=.*\S)[a-zA-Z0-9ąęćłńóśźżĄĘĆŁŃÓŚŹŻ ]*$/, "Only letters, numbers and spaces allowed")
+        .regex(/^$|^(?=.*\S)[a-zA-Z0-9ąęćłńóśźżĄĘĆŁŃÓŚŹŻ ]*$/, "Only letters, numbers and spaces allowed")
         .optional()
         .or(z.literal(''))
 });
