fbd8e1d3b4f7fabee784587ddcd599500c1233a1
Emilia Kura <emillia.kura@gmail.com>
2026-08-19 21:40:22 +0200

    feat: add loading state

--- src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
+++ src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
@@ -3,7 +3,8 @@ import {useTheme} from "@/src/context/ModeContext";
 import {useState} from "react";
 import {TrustedUserDetailsResponse} from "@/src/api/dto/response/TrustedUserDetailsResponse";
 import {Colors} from "@/src/constants/theme";
-import {StyleSheet} from "react-native";
+import {ActivityIndicator, StyleSheet, Text, View} from "react-native";
+import BottomSheet from "@/src/components/ui/BottomSheet";
 
 const formSchema = z.object({
     nickname: z.string()
@@ -30,13 +31,34 @@ export default function ManageTrustedUserSheet({isVisible, trustedUserId, onClos
 
     // Api data
     const [userDetails, setUserDetails] = useState<TrustedUserDetailsResponse | null>(null);
+
+    return (
+        <BottomSheet
+            isVisible={isVisible}
+            onClose={onClose}
+        >
+            <View style={styles.content}>
+                {isLoading || !userDetails ? (
+                    // When data is not ready
+                    <View style={styles.loadingContainer}>
+                        <ActivityIndicator size={"large"} color={theme.tint}/>
+                    </View>
+                ) : (
+                    // Target form
+                    <>
+                        <Text style={styles.title}>Manage User</Text>
+                    </>
+                )}
+            </View>
+        </BottomSheet>
+    );
 }
 
 const styles = StyleSheet.create({
     content: {
         padding: 24,
     },
-    container: {
+    loadingContainer: {
         paddingVertical: 40,
         alignItems: 'center',
         justifyContent: 'center',
