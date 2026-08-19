08e605cd80cbb856aa6588a5db4fb207406ca93f
Emilia Kura <emillia.kura@gmail.com>
2026-08-19 21:26:02 +0200

    feat: add formSchema

--- src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
+++ src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
@@ -1,6 +1,19 @@
+import {z} from "zod";
+
+const formSchema = z.object({
+    nickname: z.string()
+        .max(100, "Nickname cannot be longer than 100 characters")
+        .regex(/^(?=.*\S)[a-zA-Z0-9ąęćłńóśźżĄĘĆŁŃÓŚŹŻ ]*$/, "Only letters, numbers and spaces allowed")
+        .optional()
+        .or(z.literal(''))
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
 interface ManageTrustedUserSheetProps {
     isVisible: boolean;
-    trustedUserId: number;
+    trustedUserId: number | null;
     onClose: () => void;
     onSuccess: () => void;
 }
+
