bb7579a27517a393fdf49bd0c2cb351904424d61
Emilia Kura <emillia.kura@gmail.com>
2026-08-19 13:05:34 +0200

    feat: add NotificationListProps

---
+++ src/components/dashboard/trusted-user/notifications/NotificationList.tsx
@@ -0,0 +1,9 @@
+import {NotificationListResponse} from "@/src/api/dto/response/NotificationListResponse";
+
+interface NotificationListProps {
+    notifications: NotificationListResponse[];
+}
+
+export default function NotificationList({notifications}: NotificationListProps) {
+
+}
