0994c9f08fe33896dc8f621619879da0063552a5
Emilia Kura <emillia.kura@gmail.com>
2026-08-19 13:06:37 +0200

    feat: add styles

--- src/components/dashboard/trusted-user/notifications/NotificationList.tsx
+++ src/components/dashboard/trusted-user/notifications/NotificationList.tsx
@@ -1,4 +1,5 @@
 import {NotificationListResponse} from "@/src/api/dto/response/NotificationListResponse";
+import {StyleSheet} from "react-native";
 
 interface NotificationListProps {
     notifications: NotificationListResponse[];
@@ -7,3 +8,9 @@ interface NotificationListProps {
 export default function NotificationList({notifications}: NotificationListProps) {
 
 }
+
+const styles = StyleSheet.create({
+    listContainer: {
+        width: '100%',
+    },
+});
