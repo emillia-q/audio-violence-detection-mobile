3c75f5fb96334b4142fd7fd8fb4edcdbebfcd3fb
Emilia Kura <emillia.kura@gmail.com>
2026-08-19 21:30:22 +0200

    feat: add styles

--- src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
+++ src/components/dashboard/user/trusted-users/ManageTrustedUserSheet.tsx
@@ -1,4 +1,9 @@
 import {z} from "zod";
+import {useTheme} from "@/src/context/ModeContext";
+import {useState} from "react";
+import {TrustedUserDetailsResponse} from "@/src/api/dto/response/TrustedUserDetailsResponse";
+import {Colors} from "@/src/constants/theme";
+import {StyleSheet} from "react-native";
 
 const formSchema = z.object({
     nickname: z.string()
@@ -17,3 +22,29 @@ interface ManageTrustedUserSheetProps {
     onSuccess: () => void;
 }
 
+export default function ManageTrustedUserSheet({isVisible, trustedUserId, onClose, onSuccess}: ManageTrustedUserSheetProps) {
+    const theme = useTheme();
+
+    // Load
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Api data
+    const [userDetails, setUserDetails] = useState<TrustedUserDetailsResponse | null>(null);
+}
+
+const styles = StyleSheet.create({
+    content: {
+        padding: 24,
+    },
+    container: {
+        paddingVertical: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '700',
+        marginBottom: 8,
+        color: Colors.user.text,
+    },
+});
