1ff98a8c7543495125aab09b4d8780a91339abd8
Emilia Kura <emillia.kura@gmail.com>
2026-08-20 14:24:40 +0200

    feat: add styles

--- src/components/dashboard/user/alerts/ManageAlertSheet.tsx
+++ src/components/dashboard/user/alerts/ManageAlertSheet.tsx
@@ -1,6 +1,33 @@
+import {useTheme} from "@/src/context/ModeContext";
+import {StyleSheet} from "react-native";
+
 interface ManageAlertSheetProps {
     isVisible: boolean;
     alertId: number | null;
     onClose: () => void;
     onSuccess: () => void;
 }
+
+export default function ManageAlertSheet({isVisible, alertId, onClose, onSuccess}: ManageAlertSheetProps) {
+    const theme = useTheme();
+
+
+}
+
+const styles = StyleSheet.create({
+    content: {
+        padding: 24,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '700',
+        marginBottom: 4,
+    },
+    subtitle: {
+        fontSize: 14,
+        marginBottom: 24,
+    },
+    button: {
+        gap: 16,
+    },
+});
