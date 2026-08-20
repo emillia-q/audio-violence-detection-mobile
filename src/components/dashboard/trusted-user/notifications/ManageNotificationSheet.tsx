73a376fa2b04879f6a4583b13411ed8b085f922a
Emilia Kura <emillia.kura@gmail.com>
2026-08-20 14:53:03 +0200

    feat: add styles

--- src/components/dashboard/trusted-user/notifications/ManageNotificationSheet.tsx
+++ src/components/dashboard/trusted-user/notifications/ManageNotificationSheet.tsx
@@ -1,3 +1,5 @@
+import {StyleSheet} from "react-native";
+
 interface ManageNotificationSheetProps {
     isVisible: boolean;
     notificationId: number | null;
@@ -5,3 +7,25 @@ interface ManageNotificationSheetProps {
     onClose: () => void;
     onSuccess: () => void;
 }
+
+export default function ManageNotificationSheet({isVisible, notificationId, isRead, onClose, onSuccess}: ManageNotificationSheetProps) {
+
+}
+
+const styles = StyleSheet.create({
+    content: {
+        padding: 24,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '700',
+        marginBottom: 4,
+    },
+    subtitle: {
+        fontSize: 14,
+        marginBottom: 24,
+    },
+    button: {
+        gap: 16,
+    },
+});
