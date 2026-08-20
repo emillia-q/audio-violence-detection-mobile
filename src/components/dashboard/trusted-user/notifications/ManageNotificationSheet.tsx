338ffda24a8f09974ff31adbbaace764a8aaed2a
Emilia Kura <emillia.kura@gmail.com>
2026-08-20 14:58:08 +0200

    feat: add handleToggle function

--- src/components/dashboard/trusted-user/notifications/ManageNotificationSheet.tsx
+++ src/components/dashboard/trusted-user/notifications/ManageNotificationSheet.tsx
@@ -1,4 +1,7 @@
 import {StyleSheet} from "react-native";
+import {useTheme} from "@/src/context/ModeContext";
+import {notificationService} from "@/src/api/service/notification";
+import Toast from "react-native-toast-message";
 
 interface ManageNotificationSheetProps {
     isVisible: boolean;
@@ -9,7 +12,27 @@ interface ManageNotificationSheetProps {
 }
 
 export default function ManageNotificationSheet({isVisible, notificationId, isRead, onClose, onSuccess}: ManageNotificationSheetProps) {
+    const theme = useTheme();
 
+    // Toggle isRead flag
+    const handleToggle = async () => {
+        if (!notificationId)
+            return;
+
+        try {
+            await notificationService.toggleNotificationStatus(notificationId);
+            onSuccess();
+            onClose();
+        } catch (error: any) {
+            const status = error?.response?.status;
+
+            if (status === 404) {
+                Toast.show({type: 'error', text1: 'Notification not found'});
+            } else {
+                Toast.show({type: 'error', text1: 'Failed to delete notification'});
+            }
+        }
+    };
 }
 
 const styles = StyleSheet.create({
