22c7050d6b3c4865bbdef4ff5c54225189311b1d
Emilia Kura <emillia.kura@gmail.com>
2026-08-22 17:40:56 +0200

    feat: check if permission exists

--- src/components/dashboard/user/devices/DeviceScanner.tsx
+++ src/components/dashboard/user/devices/DeviceScanner.tsx
@@ -1,5 +1,37 @@
+import {useTheme} from "@/src/context/ModeContext";
+import {useCameraPermissions} from "expo-camera";
+import {StyleSheet, Text, View} from "react-native";
+
 interface DeviceScannerProps {
     onScan: (macAddress: string, deviceSecret: string) => void;
     onSwitchToManual: () => void;
 }
 
+export default function DeviceScanner({onScan, onSwitchToManual}: DeviceScannerProps) {
+    const theme = useTheme();
+    const [permission, requestPermission] = useCameraPermissions();
+
+    // Check if permission exists
+    if (!permission) {
+        return (
+            <View style={styles.centered}>
+                <Text style={{color: theme.text}}>Requesting camera permission</Text>
+            </View>
+        );
+    }
+
+    // If the user has not yet granted permissions
+    if (!permission.granted) {
+
+    }
+}
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 24,
+        gap: 16,
+    },
+});
