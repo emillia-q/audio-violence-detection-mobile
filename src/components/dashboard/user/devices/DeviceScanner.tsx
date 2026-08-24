d5a21495bf481deecec162655d27ae108c4b6385
Emilia Kura <emillia.kura@gmail.com>
2026-08-24 11:25:12 +0200

    feat: add grant permission section

--- src/components/dashboard/user/devices/DeviceScanner.tsx
+++ src/components/dashboard/user/devices/DeviceScanner.tsx
@@ -1,6 +1,7 @@
 import {useTheme} from "@/src/context/ModeContext";
 import {useCameraPermissions} from "expo-camera";
 import {StyleSheet, Text, View} from "react-native";
+import {CustomButton} from "@/src/components/ui/CustomButton";
 
 interface DeviceScannerProps {
     onScan: (macAddress: string, deviceSecret: string) => void;
@@ -22,7 +23,20 @@ export default function DeviceScanner({onScan, onSwitchToManual}: DeviceScannerP
 
     // If the user has not yet granted permissions
     if (!permission.granted) {
-
+        return (
+            <View style={styles.centered}>
+                <Text style={[styles.message, {color: theme.text}]}>We need your permission to show the camera</Text>
+                <CustomButton
+                    title={"Grant permission"}
+                    onPress={requestPermission}
+                />
+                <CustomButton
+                    title={"Enter manually instead"}
+                    variant={"text"}
+                    onPress={onSwitchToManual}
+                />
+            </View>
+        );
     }
 }
 
@@ -33,5 +47,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 24,
         gap: 16,
+    },
+    message: {
+        textAlign: 'center',
+        fontSize: 16,
     },
 });
