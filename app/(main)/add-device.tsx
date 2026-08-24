c8ba8e9fd9124a8075c7fff7da67424d6b6a8958
Emilia Kura <emillia.kura@gmail.com>
2026-08-24 12:18:52 +0200

    feat: add handlePairDevice function & add DeviceScanner

--- app/(main)/add-device.tsx
+++ app/(main)/add-device.tsx
@@ -3,21 +3,68 @@ import {StyleSheet, Text, View} from "react-native";
 import {useRouter} from "expo-router";
 import {CustomButton} from "@/src/components/ui/CustomButton";
 import {useTheme} from "@/src/context/ModeContext";
+import {useState} from "react";
+import {deviceService} from "@/src/api/service/device";
+import Toast from "react-native-toast-message";
+import DeviceScanner from "@/src/components/dashboard/user/devices/DeviceScanner";
 
 export default function AddDeviceScreen() {
     const router = useRouter();
     const theme = useTheme();
 
+    const [isPairing, setIsPairing] = useState(false);
+
+    const handlePairDevice = async (macAddress: string, deviceSecret: string) => {
+        // Double scan protection
+        if (isPairing)
+            return;
+
+        try {
+            await deviceService.pairDevice({macAddress, deviceSecret});
+
+            Toast.show({
+                type: 'success',
+                text1: 'Device paired successfully'
+            });
+
+            // Back to dashboard
+            router.back();
+        } catch (error: any) {
+            const status = error?.response?.status;
+
+            if (status === 404) {
+                Toast.show({
+                    type: 'error',
+                    text1: 'Device not found',
+                    text2: 'Invalid MAC Address'
+                });
+            } else if (status === 401) {
+                Toast.show({
+                    type: 'error',
+                    text1: 'Invalid device credentials'
+                });
+            } else if (status === 409) {
+                Toast.show({
+                    type: 'error',
+                    text1: 'Device is already assigned to a user'
+                });
+            } else {
+                Toast.show({
+                    type: 'error',
+                    text1: 'Failed to pair device'
+                });
+            }
+        } finally {
+            setIsPairing(false);
+        }
+    };
+
     return (
         <SafeAreaView style={[styles.safeArea, {backgroundColor: theme.background}]}>
-            <View style={styles.content}>
-                <Text style={[styles.title, {color: theme.text}]}>Add new device</Text>
-                <CustomButton
-                    title="Back to dashboard"
-                    variant="outline"
-                    onPress={() => router.back()}
+                <DeviceScanner
+                    onScan={handlePairDevice}
+                    onSwitchToManual={() => console.log("manual")}
                 />
-            </View>
         </SafeAreaView>
     );
 }
