ead7b58207e1c7c328d3eb378af11d26a628d549
Emilia Kura <emillia.kura@gmail.com>
2026-08-25 14:06:09 +0200

    feat: add styles

--- src/components/dashboard/user/devices/ManageDeviceSheet.tsx
+++ src/components/dashboard/user/devices/ManageDeviceSheet.tsx
@@ -1,6 +1,24 @@
+import {StyleSheet} from "react-native";
+
 interface ManageDeviceSheetProps {
     isVisible: boolean;
     deviceId: number | null;
     onClose: () => void;
     onSuccess: () => void;
 }
+
+const styles = StyleSheet.create({
+    content: {
+        padding: 24,
+    },
+    loadingContainer: {
+        paddingVertical: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '700',
+        marginBottom: 8,
+    },
+});
