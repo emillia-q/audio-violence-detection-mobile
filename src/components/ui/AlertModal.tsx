d35e8727ddbc9d1b2ba2ba990d2f69544fc54afc
Emilia Kura <emillia.kura@gmail.com>
2026-08-31 12:16:25 +0200

    feat: add functions to props

--- src/components/ui/AlertModal.tsx
+++ src/components/ui/AlertModal.tsx
@@ -5,9 +5,21 @@ interface AlertModalProps {
     title: string;
     message: string;
     isVisible: boolean;
+    onCancel: () => void;
+    onConfirm: () => void;
+    cancelText?: string;
+    confirmText?: string;
 }
 
-export default function AlertModal({title, message, isVisible}: AlertModalProps) {
+export default function AlertModal({
+                                       title,
+                                       message,
+                                       isVisible,
+                                       onCancel,
+                                       onConfirm,
+                                       cancelText = "Cancel",
+                                       confirmText = "Confirm"
+                                   }: AlertModalProps) {
     const theme = useTheme();
 
     return (
@@ -21,6 +33,4 @@ export default function AlertModal({title, message, isVisible}: AlertModalProps)
     );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
