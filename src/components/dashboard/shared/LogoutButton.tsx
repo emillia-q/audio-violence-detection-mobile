2fea36f1e31fbe8bb0e1db5a76972266854c274f
Emilia Kura <emillia.kura@gmail.com>
2026-08-31 12:34:14 +0200

    refactor: add custom alert modal

--- src/components/dashboard/shared/LogoutButton.tsx
+++ src/components/dashboard/shared/LogoutButton.tsx
@@ -1,6 +1,8 @@
 import {useTheme} from "@/src/context/ModeContext";
-import {Alert, StyleSheet, TouchableOpacity} from "react-native";
+import {StyleSheet, TouchableOpacity} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
+import {useState} from "react";
+import AlertModal from "@/src/components/ui/AlertModal";
 
 interface LogoutButtonProps {
     onLogout: () => void;
@@ -8,33 +10,42 @@ interface LogoutButtonProps {
 
 export default function LogoutButton({onLogout}: LogoutButtonProps) {
     const theme = useTheme();
+    const [isAlertModalVisible, setIsAlertModalVisible] = useState(false);
 
     const handlePress = () => {
-        Alert.alert(
-            "Log Out",
-            "Are you sure you want to log out?",
-            [
-                {
-                    text: "Cancel",
-                    style: "cancel"
-                },
-                {
-                    text: "Log Out",
-                    style: "destructive",
-                    onPress: onLogout
-                }
-            ]
-        );
+        setIsAlertModalVisible(true);
+    };
+
+    const handleConfirm = () => {
+        setIsAlertModalVisible(false);
+        onLogout();
+    };
+
+    const handleCancel = () => {
+        setIsAlertModalVisible(false);
     };
 
     return (
-        <TouchableOpacity
-            onPress={handlePress}
-            style={styles.container}
-            activeOpacity={0.7}
-        >
-            <Ionicons name={"log-out-outline"} size={32} color={theme.text}/>
-        </TouchableOpacity>
+        <>
+            <TouchableOpacity
+                onPress={handlePress}
+                style={styles.container}
+                activeOpacity={0.7}
+            >
+                <Ionicons name={"log-out-outline"} size={32} color={theme.text}/>
+            </TouchableOpacity>
+
+            {/* Alert Modal */}
+            <AlertModal
+                title={"Log Out"}
+                message={"Are you sure you want to log out?"}
+                isVisible={isAlertModalVisible}
+                cancelText={"Cancel"}
+                confirmText={"Log Out"}
+                onCancel={handleCancel}
+                onConfirm={handleConfirm}
+            />
+        </>
     );
 }
 
