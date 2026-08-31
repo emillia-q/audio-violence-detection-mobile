495635f765738a1111e67b390ef54249875eaa79
Emilia Kura <emillia.kura@gmail.com>
2026-08-31 12:14:08 +0200

    feat: add default function and styles

--- src/components/ui/AlertModal.tsx
+++ src/components/ui/AlertModal.tsx
@@ -1,5 +1,26 @@
+import {useTheme} from "@/src/context/ModeContext";
+import {Modal, StyleSheet} from "react-native";
+
 interface AlertModalProps {
     title: string;
     message: string;
     isVisible: boolean;
 }
+
+export default function AlertModal({title, message, isVisible}: AlertModalProps) {
+    const theme = useTheme();
+
+    return (
+        <Modal
+            visible={isVisible}
+            transparent={true}
+            animationType={"fade"}
+        >
+
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
