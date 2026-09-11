cfa548eaebcbc0c57a6648f9fb6a872408f06776
Emilia Kura <emillia.kura@gmail.com>
2026-09-11 13:41:25 +0200

    feat: add fetchAlerts function and refresh the screen each time the page changes

--- app/(main)/alert-history.tsx
+++ app/(main)/alert-history.tsx
@@ -1,5 +1,34 @@
+import {useTheme} from "@/src/context/ModeContext";
+import {useEffect, useState} from "react";
+import {AlertListResponse} from "@/src/api/dto/response/AlertListResponse";
+import {alertService} from "@/src/api/service/alert";
+import Toast from "react-native-toast-message";
+
 const PAGE_SIZE = 10;
 
 export default function AlertHistoryScreen() {
+    const theme = useTheme();
 
+    const [page, setPage] = useState(0);
+    const [alerts, setAlerts] = useState<AlertListResponse[]>([]);
+    const [isManageAlertVisible, setIsManageAlertVisible] = useState(false);
+    const [selectedAlertId, setSelectedAlertId] = useState<number | null>(null);
+    const [selectedAlertIsRead, setSelectedAlertIsRead] = useState(false);
+
+    const fetchAlerts = async (pageNumber: number) => {
+        try {
+            const data = await alertService.getListOfAlerts(pageNumber, PAGE_SIZE);
+            setAlerts(data);
+        } catch (error) {
+            Toast.show({
+                type: 'error',
+                text1: 'Error fetching alerts',
+            });
+        }
+    }
+
+    // Refresh each time teh page changes
+    useEffect(() => {
+        fetchAlerts(page);
+    }, [page]);
 }
